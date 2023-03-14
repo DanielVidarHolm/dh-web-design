8b94325c7890c0c1eef77bfc9230a5c021b254e1
Daniel Holm <dannidona3@gmail.com>
2023-03-14 13:59:32 +0100

    styling and adding behavior to the nav

---
+++ public/main.js
@@ -0,0 +1,4 @@
+document.querySelector('#hamburger').addEventListener('click', ()=>{
+    document.querySelector('#navtarget').style.position = "absolute"
+    document.querySelector('#navtarget').style.display = "block"
+})

b3b131dc158d1c6b846380b9a60a1b386fff43fa
Daniel Holm <dannidona3@gmail.com>
2023-03-15 08:26:13 +0100

    fixed navigation hamburger click event

--- public/main.js
+++ public/main.js
@@ -1,32 +1,6 @@
-
-let isNavVisible = true
-let isNavVisibleMobile = true
 document.querySelector('#hamburger').addEventListener('click', ()=>{
-    const target = document.querySelector('#navtarget')
-    if(isNavVisible === true){
-        target.style.display = 'block'
-        isNavVisible = false
-        isNavVisibleMobile = false
-    }else{
-        target.style.display = 'none'
-        isNavVisible = true
-        isNavVisibleMobile = true
-    }
-
-});
-
-window.addEventListener('resize', ()=> {
-    if(window.innerWidth > 992){
-        document.querySelector('#navtarget').style.display = "flex"
-        isNavVisible = false
-        isNavVisibleMobile = false
-    }else if(window.innerWidth <= 992 && isNavVisibleMobile) {
-        document.querySelector('#navtarget').style.display = "none"
-        isNavVisible = true
-        
-    }
+    document.querySelector('#navtarget').classList.toggle('show')
 })
-// Resizing stuff
 
 
 

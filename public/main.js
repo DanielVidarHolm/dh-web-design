7f9a055397111b732eeb1d6b690338b32c13193c
Daniel Vidar Holm <dannidona3@gmail.com>
2023-03-15 06:39:50 +0100

    sdf

--- public/main.js
+++ public/main.js
@@ -1,4 +1,32 @@
+
+let isNavVisible = true
+let isNavVisibleMobile = true
 document.querySelector('#hamburger').addEventListener('click', ()=>{
-    document.querySelector('#navtarget').style.position = "absolute"
-    document.querySelector('#navtarget').style.display = "block"
+    const target = document.querySelector('#navtarget')
+    if(isNavVisible === true){
+        target.style.display = 'block'
+        isNavVisible = false
+        isNavVisibleMobile = false
+    }else{
+        target.style.display = 'none'
+        isNavVisible = true
+        isNavVisibleMobile = true
+    }
+
+});
+
+window.addEventListener('resize', ()=> {
+    if(window.innerWidth > 992){
+        document.querySelector('#navtarget').style.display = "flex"
+        isNavVisible = false
+        isNavVisibleMobile = false
+    }else if(window.innerWidth <= 992 && isNavVisibleMobile) {
+        document.querySelector('#navtarget').style.display = "none"
+        isNavVisible = true
+        
+    }
 })
+// Resizing stuff
+
+
+

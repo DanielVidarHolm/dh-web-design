792a8bbc942627a766807b1b56c4a1b9e35db060
Daniel Holm <dannidona3@gmail.com>
2023-03-15 11:09:19 +0100

    Added an accordion on nav menu

--- public/main.js
+++ public/main.js
@@ -1,5 +1,16 @@
 document.querySelector('#hamburger').addEventListener('click', ()=>{
-    document.querySelector('#navtarget').classList.toggle('show')
+    if (document.querySelector('#navtarget').offsetHeight > 0 ){
+        document.querySelector('#navtarget').style.height = 0 +"px"
+    }else {
+        const navItems = document.querySelectorAll("#navtarget > .nav-item")
+        let total = 0
+        for (let i = 0; i < navItems.length; i++){
+            total += navItems[i].offsetHeight
+        }
+        document.querySelector('#navtarget').style.height = total + "px"
+    }
+    
+    
 })
 
 

2ad75131fdf71b01495f14d16ba78360a4c9e764
Daniel Vidar Holm <dannidona3@gmail.com>
2023-03-18 09:50:47 +0100

    rebuild

--- public/main.js
+++ public/main.js
@@ -1,19 +1,3 @@
 
-// Accordiom funtion on the nav menu
-document.querySelector('#hamburger').addEventListener('click', ()=>{
-    if (document.querySelector('#navtarget').offsetHeight > 0 ){
-        document.querySelector('#navtarget').style.height = 0 +"px"
-    }else {
-        const navItems = document.querySelectorAll("#navtarget > .nav-item")
-        let total = 0
-        for (let i = 0; i < navItems.length; i++){
-            total += navItems[i].offsetHeight
-        }
-        document.querySelector('#navtarget').style.height = total + "px"
-    }
-    
-    
-})
-
 
 
